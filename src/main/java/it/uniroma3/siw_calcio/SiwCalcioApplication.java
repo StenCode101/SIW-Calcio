19651000de3457babe5614380a7c5010249b6d2b
StenCode101 <stefped04@gmail.com>
2026-07-27 11:47:52 +0200

    inizio

---
+++ src/main/java/it/uniroma3/siw_calcio/SiwCalcioApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw_calcio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwCalcioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwCalcioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/uniroma3/siw_calcio/SiwCalcioApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw_calcio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiwCalcioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
